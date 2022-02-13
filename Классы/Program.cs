b3cca7fd9d61cba2fab09510674a9e72819057d5
dimankan <dimankan94@mail.ru>
2022-02-13 14:13:02 +0300

    Статический метод

--- Классы/Program.cs
+++ Классы/Program.cs
@@ -6,7 +6,39 @@ namespace Классы
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car bmw = new Car("x6", 5000000);
+            Car vw = new Car("Passat", 3000000);
+
+            bmw.ShowInfo();
+            vw.ShowInfo();
+
+            Car.CalcPlusShow(5,6);
+            StaticMethods.CalcMinusShow(5,6);
+            StaticMethods.CalcMultiplayShow(5,6);
+        }
+    }
+
+    class Car
+    {
+        private string _model;
+        int _price;
+
+        public Car(string x, int y)
+        {
+            _model = x;
+            _price = y;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Модель: {_model}, стоит: {_price}");
+        }
+
+
+        public static void CalcPlusShow(int x, int y)
+        {
+            //Console.WriteLine($"{x+y}");
+            Console.WriteLine($"{x + y}");
         }
     }
 }
---
+++ Классы/StaticMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Классы
+{
+    public static class StaticMethods
+    {
+        public static void CalcPlusShow(int x, int y)
+        {
+            //Console.WriteLine($"{x+y}");
+            Console.WriteLine($"{x + y}");
+        }
+
+        public static void CalcMinusShow(int x, int y)
+        {
+            //Console.WriteLine($"{x+y}");
+            Console.WriteLine($"{x - y}");
+        }
+        public static void CalcMultiplayShow(int x, int y)
+        {
+            //Console.WriteLine($"{x+y}");
+            Console.WriteLine($"{x * y}");
+        }
+
+    }
+}
