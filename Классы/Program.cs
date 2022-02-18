0b415d16bfec5a4db54246d6e8fe5e68d5c76ec3
dimankan <dimankan94@mail.ru>
2022-02-18 19:08:34 +0300

    Перетащили класс

--- Классы/Program.cs
+++ Классы/Program.cs
@@ -8,7 +8,9 @@ namespace Классы
     {
         static void Main(string[] args)
         {
-            People artem = new People();
+            #region Работа с классом People
+            /*
+              People artem = new People();
             artem.Name = "Dima";
             artem.Sname = "Vor";
             artem.Yers = 33;
@@ -45,55 +47,13 @@ namespace Классы
             {
                 Console.WriteLine(peoples1[i].Yers);
             }
+             */
+            #endregion
+
 
         }
     }
-    class People
-    {
-        private string _name;
-        string _sname;
-        public int yers;
-
-
-        public string Name
-        {
-            get { return _name; }
-            set { _name = value; }
-        }
-        public string Sname
-        {
-            get { return _sname; }
-            set { _sname = value; }
-        }
-        public int Yers
-        {
-            get { return yers; }
-            set
-            {
-                if (value < 0 || value > 120)
-                {
-                    Console.WriteLine("Столько люди не живут!");
-                }
-                else
-                {
-                    yers = value;
-                }
-            }
-        }
-        public int SalaRy { get; set; }
-        public People(string a = "неизвестно", string b = "неизвестно", int c = 0, int d = 0)
-        {
-            Name = a;
-            Sname = b;
-            Yers = c;
-            SalaRy = d;
-        }
-
-        public void Show()
-        {
-            Console.WriteLine($"Имя:{_name} Фамилия:{_sname} Возраст:{yers} Ваша зп:{SalaRy}");
-        }
-    }
+   
 }
 
 #region Черновик
