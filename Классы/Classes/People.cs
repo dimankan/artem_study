56da6569492afc1d311bac41e9693e042b36452b
dimankan <dimankan94@mail.ru>
2022-03-07 16:12:59 +0300

    Add Winforms. New folders

--- Классы/Classes/People.cs
+++ Классы/Classes/People.cs
@@ -6,48 +6,6 @@ using System.Threading.Tasks;
 
 namespace Классы
 {
-
-    #region Работа с классом People
-    /*
-      People artem = new People();
-    artem.Name = "Dima";
-    artem.Sname = "Vor";
-    artem.Yers = 33;
-    artem.Show();
-
-    People people = new People("Artem");
-   // people.Yers = 155; 
-    people.Show();
-
-    People people1 = new People("Artem", "Petrov");
-    people1.Show();
-
-    People people2 = new People("Artem", "Petrov", 150);
-    people2.Show();
-
-    People people3 = new People("Vanya", "Grigorev", 26, 45500);
-    people3.Show();
-
-    People[] peoples = new People[] {artem, people1, people2, people3};
-    foreach (People item in peoples)
-    {
-        Console.WriteLine(item.Name);
-    }
-    for (int i = 0; i < peoples.Length; i++)
-    {
-        Console.WriteLine(peoples[i].Sname);
-    }
-    List<People> peoples1 = new List<People>() { artem, people1, people2, people3 };
-    foreach (People item in peoples1)
-    {
-        Console.WriteLine(item.SalaRy);
-    }
-    for (int i = 0; i < peoples1.Count; i++)
-    {
-        Console.WriteLine(peoples1[i].Yers);
-    }
-     */
-    #endregion
     class People
     {
         private string _name;
--- Классы/Program.cs
+++ Классы/Program.cs
@@ -1,5 +1,7 @@
-﻿using Meth;
+﻿using ConsoleArt.Classes;
+using Meth;
 using System;
+
 using System.Collections.Generic;
 
 namespace Классы
@@ -8,127 +10,17 @@ namespace Классы
     {
         static void Main(string[] args)
         {
-            int x = SumMul(5, 20);
-            Console.WriteLine(x);
-        }
-        public static bool isDivisible(long n, long x, long y)
-        {
-            bool result = false;
-            bool a = x % n == 0;
-            bool b = y % n == 0;
-            if (a && b)
-            {
-                result = true;
-            }
-
-            return result;
-        }
-        public static int[] CountPositivesSumNegatives(int[] input)
-        {
-
-            if (input == null)
-            {
-                return new int[0] { };
-            }
-
-            else if (input.Length == 0)
-            {
-                return new int[0] { };
-            }
-
-            int[] result = { 0, 0 };
-
-            foreach (var item in input)
-            {
-                if (item > 0)
-                {
-                    result[0] += 1;
-                }
-                else if (item < 0)
-                {
-                    result[1] += item;
-                }
-
-            }
-            return result;
-
+            #region InvokeRun
+            //RunCodeWars();
+            //RunPeople();
+            //RunCar(); 
+            #endregion
 
         }
-        public static int Remainder(int a, int b)
-        {
-           int result = 0;
-            if (a > b)
-            {
-                result = a % b;
-            }
-            else if (a < b)
-            {
-                result = b % a;
-            }
-            else
-            {
-                throw new DivideByZeroException();
-            }
-            
 
-            return result;
-        }
-        public static string CannonsReady(Dictionary<string, string> gunners)
-        {
-            string result = "";
-            int count = gunners.Count;
-            int countReady = 0;
-            foreach (var item in gunners)
-            {
-                if (item.Value == "aye")
-                {
-                    countReady++;
-                }
-            }
-            if (countReady == count)
-            {
-                result = "Fire!";
-            }
-            else
-            {
-                result = "Shiver me timbers!";
-            }
-            return result;
-        }
+        #region RunMethods
 
-        public static int SumMul(int n, int m)
-        {
-           if (n == 0)
-            {
-                throw new ArgumentException();
-            }
-           if (n > m)
-            {
-                throw new ArgumentException();
-            }
-           int result = 0;
-            int temp = n;
-            while (true)
-            {
-                if (m < temp)
-                {
-                    break;
-                }
-                result += temp;
-                temp += n;
-            }
-            return result;
-        }
-    }
-
-
-}
-
-#region Черновик
-/*
-  internal class Program
-    {
-        static void Main(string[] args)
+        private static void RunCar()
         {
             Car bmw = new Car("x6", 5000000);
             Car vw = new Car("Passat", 3000000);
@@ -140,58 +32,57 @@ namespace Классы
 
             //bmw.ShowInfo();
             //vw.ShowInfo();
+        }
+        private static void RunCodeWars()
+        {
+            int x = CodeWars.SumMul(5, 20);
+            Console.WriteLine(x);
+        }
+        public static void RunPeople()
+        {
+            People artem = new People();
+            artem.Name = "Dima";
+            artem.Sname = "Vor";
+            artem.Yers = 33;
+            artem.Show();
+
+            People people = new People("Artem");
+            // people.Yers = 155; 
+            people.Show();
+
+            People people1 = new People("Artem", "Petrov");
+            people1.Show();
+
+            People people2 = new People("Artem", "Petrov", 150);
+            people2.Show();
+
+            People people3 = new People("Vanya", "Grigorev", 26, 45500);
+            people3.Show();
+
+            People[] peoples = new People[] { artem, people1, people2, people3 };
+            foreach (People item in peoples)
+            {
+                Console.WriteLine(item.Name);
+            }
+            for (int i = 0; i < peoples.Length; i++)
+            {
+                Console.WriteLine(peoples[i].Sname);
+            }
+            List<People> peoples1 = new List<People>() { artem, people1, people2, people3 };
+            foreach (People item in peoples1)
+            {
+                Console.WriteLine(item.SalaRy);
+            }
+            for (int i = 0; i < peoples1.Count; i++)
+            {
+                Console.WriteLine(peoples1[i].Yers);
+            }
 
 
         }
+
+        #endregion
+
     }
+}
 
-    class Car
-    {
-        private protected string _model = "Volvo";
-        int _price;
-        //private int _yearOfMade;
-
-
-        public int YearOfMade { get; set; }
-
-        public int Price
-        {
-            get { return _price; }
-            set { _price = value; }
-        }
-
-        public string Info
-        {
-            get { return $"{_model} стоит {_price}"; }
-        }
-
-
-        //public void ChangePrice(int x)
-        //{
-        //    _price = x;
-        //}
-
-
-
-
-        public Car(string x, int y)
-        {
-            _model = x;
-            Price = y;
-            YearOfMade = 3423;
-        }
-
-        public void ShowInfo()
-        {
-            Console.WriteLine($"Модель: {_model}, стоит: {_price}, год производства {YearOfMade}");
-        }
-
-
-        public static void CalcPlusShow(int x, int y)
-        {
-            Console.WriteLine($"{x + y}");
-        }
-    }
-
- */
-#endregion
